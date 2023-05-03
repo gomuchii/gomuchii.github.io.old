6c836982ed054cc0b82c8f7e65996dcd9000ed21
yoann <goannyautier@gmail.com>
2023-05-03 08:36:34 +0200

    test

--- push.bash
+++ push.bash
@@ -1,11 +1,6 @@
 #!/bin/bash
 git add --all
 
-git commit -m "auto commit"
+git commit -m "test"
 
-git push -u origin main
-
-gomuchii
-
-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
-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
+git push
